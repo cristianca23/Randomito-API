bef7321ea4f3b4bcf09deeab75bd7d447dde8eca
cristianca23 <cristianca231@gmail.com>
2022-02-02 20:09:17 -0500

    add lint.yml to CI

--- src/index.js
+++ src/index.js
@@ -6,20 +6,17 @@ const morgan = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan(':method :url port=${PORT} :status :res[content-length] - :response-time ms'))
+app.use(morgan(':method :url port=${PORT} :status :res[content-length] - :response-time ms'));
 
 const randomFloat = require('./useCases/randomFloat');
 
 
 app.get('/api/v1/number', (req, res) =>{
+  const value = randomFloat.randomFloat();
 
-    const value = randomFloat.randomFloat();
-
-    res.json({value});
-
-})
+  res.json({value});
+});
 
 app.listen(PORT, () =>{
-
-    console.log('Randomito API Listen on port:', PORT);
-})
+  console.log('Randomito API Listen on port:', PORT);
+});
